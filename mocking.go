44b9e14e3f07bedae2d6ce9adb01dd81bdff7ba2
Hazem El-Sayed <118324667+zomasec@users.noreply.github.com>
2024-02-17 23:11:13 +0200

    Create mocking.go

---
+++ mocking.go
@@ -0,0 +1,40 @@
+package mocking
+
+import (
+	"fmt"
+	"io"
+	"time"
+)
+
+var countdownStart int = 3
+var finalWord string = "Go!"
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpySleeper struct {
+	Calls int
+}
+
+type DefaultSleeper struct{}
+
+func (s *SpySleeper) Sleep() {
+	s.Calls++
+}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+	}
+
+	fmt.Fprint(out, finalWord)
+}
