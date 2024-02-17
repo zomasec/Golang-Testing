55780c35a90511e8a8af1a7d6149bc4229aa7808
Hazem El-Sayed <118324667+zomasec@users.noreply.github.com>
2024-02-17 23:11:59 +0200

    Create mocking_test.go

---
+++ mocking/mocking_test.go
@@ -0,0 +1,23 @@
+package mocking
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestCountdown(t *testing.T) {
+	buffer := &bytes.Buffer{}
+	spySleeper := &SpySleeper{}
+	Countdown(buffer)
+	got := buffer.String()
+	want := `3
+2
+1
+Go!`
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+	if spySleeper.Calls != 3 {
+		t.Errorf("not enough calls to sleeper, want 3 got %d", spySleeper.Calls)
+	}
+}
